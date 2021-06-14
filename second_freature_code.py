00cb6c74087a27bf2234875ce96d167e62c7bdb2
aksutikania@gmail.com <aksutikania@gmail.com>
2021-06-14 22:28:35 +0300

    'basic fixes'

--- second_freature_code.py
+++ second_freature_code.py
@@ -1,15 +1,17 @@
-# объявление функции
-def solve(a, b, c):
+# defining new function 
+def equation_roots(a, b, c):
+    import math
     if b**2-4*a*c==0:
         x1,x2=-b/(2*a),-b/(2*a)
     elif b**2-4*a*c>0:
-        x1=(-b-(b**2-4*a*c)**0.5)/(2*a)
-        x2=(-b+(b**2-4*a*c)**0.5)/(2*a)
+        x1=(-b-math.sqrt(b**2-4*a*c))/(2*a)
+        x2=(-b+math.sqrt(b**2-4*a*c))/(2*a)
         x1,x2=min(x1,x2),max(x1,x2)
     return x1,x2
-# считываем данные
+# reading inputs
 a, b, c = int(input()), int(input()), int(input())
 
-# вызываем функцию
-x1, x2 = solve(a, b, c)
+# calling the function
+x1, x2 = equation_roots(a, b, c)
 print(x1, x2)
+
