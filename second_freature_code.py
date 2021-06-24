b5748eeffd2957faf0481a9e48c040c66d8f5900
aksutikania@gmail.com <aksutikania@gmail.com>
2021-06-24 21:24:21 +0300

    the last changes

--- second_freature_code.py
+++ second_freature_code.py
@@ -6,31 +6,26 @@ def equation_roots(a, b, c):
   d=b**2-4*a*c #d-equation discriminant
   if d==0:
     x1,x2=-b/(2*a),-b/(2*a)
-    return x1,x2
   elif d>0:
     x1=(-b-math.sqrt(d))/(2*a)
     x2=(-b+math.sqrt(d))/(2*a)
     x1,x2=min(x1,x2),max(x1,x2)
-    return x1,x2
   else:
     x1=None
     x2=None
-    return x1,x2
- 
-# reading and cheking inputs
-while True:
-    a,b,c = input(),input(),input()
-    try:
-        a,b,c=float(a),float(b),float(c)
-        break
-    except ValueError:
-       print("Some inputs are not numbers.Please, try again")
-
-
+  return x1,x2
+    
 # calling the function
 if __name__=="__main__":
+  # reading and cheking inputs
+  while True:
+      a,b,c = input(),input(),input()
+      try:
+          a,b,c=float(a),float(b),float(c)
+          break
+      except ValueError:
+        print("Some inputs are not numbers.Please, try again")
   x1,x2=equation_roots(a, b, c)
   print(x1, x2)
 else:
   print('Variables are not integer')
-
