4e9794d88940b28b04115c05cf5562df2706c939
aksutikania@gmail.com <aksutikania@gmail.com>
2021-07-01 23:21:15 +0300

    Added codes for four test cases

--- tests.py
+++ tests.py
@@ -1,42 +1,40 @@
 from unittest import TestCase
 from quadratic_roots import equation_roots
+import random
 
-NUMBER_RANDOM_CASES = 1000
-
-
-def plus_numbers(a, b):
-    return a + b
+NUMBER_RANDOM_CASES = 100
 
 
 class QuadraticEquation(TestCase):
-    def test_plus_numbers(self):
-        # TODO: replace it or remove unused code
-        first_number = 45
-        second_number = 34
-        required_result = 79
-        function_result = plus_numbers(first_number, second_number)
-        self.assertEqual(required_result, function_result)
 
     def test_determinant_zero(self):
         a, b, c = 1, 2, 1
         correct_roots = [-1, -1]
         x1, x2 = equation_roots(a, b, c)
         # TODO: replace to assertEqual
-        self.assertTrue(correct_roots == [x1, x2])
+        self.assertEquals([x1, x2], correct_roots)
 
     def test_determinant_negative(self):
         # TODO: test negative determinant
+        a, b, c, = 5, 8, 4
+        x1, x2 = equation_roots(a, b, c)
         # TODO: use assertIsNone
-        self.assertTrue(True)
+        self.assertIsNone(x1, x2)
+        pass
 
     def test_determinant_positive(self):
         # TODO: test positive determinant
-        self.assertTrue(True)
+        a, b, c = 2, 7, -4
+        x1, x2 = equation_roots(a, b, c)
+        correct_roots = [-4.0, 0.5]
+        self.assertEquals([x1, x2], correct_roots)
 
     def test_random_parameters(self):
         for i in range(NUMBER_RANDOM_CASES):
             # TODO: generate random a, b, c
-            # get roots
-            # check quadratic equation equals zero
-            # try to use
-            pass
+            a, b, c = random.uniform(-10, 10), random.uniform(-10, 10), random.uniform(-10, 10)
+            x1, x2 = equation_roots(a, b, c)
+            if x1 is not None:
+                equation_answer_1 = a * (x1 ** 2) + b * x1 + c
+                equation_answer_2 = a * (x2 ** 2) + b * x2 + c
+                self.assertAlmostEquals(equation_answer_1, equation_answer_2, 0)
