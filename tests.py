365a6d1312474f37b8b93ac9759f074282dad90a
Ihar Kryvulets <ikrivulets@fb.com>
2021-07-13 13:54:07 +0100

    Completed and fixed tests.py Added more checkers

--- tests.py
+++ tests.py
@@ -11,19 +11,14 @@ class QuadraticEquation(TestCase):
         a, b, c = 1, 2, 1
         correct_roots = [-1, -1]
         x1, x2 = equation_roots(a, b, c)
-        # TODO: replace to assertEqual
         self.assertEquals([x1, x2], correct_roots)
 
     def test_determinant_negative(self):
-        # TODO: test negative determinant
         a, b, c, = 5, 8, 4
         x1, x2 = equation_roots(a, b, c)
-        # TODO: use assertIsNone
         self.assertIsNone(x1, x2)
-        pass
 
     def test_determinant_positive(self):
-        # TODO: test positive determinant
         a, b, c = 2, 7, -4
         x1, x2 = equation_roots(a, b, c)
         correct_roots = [-4.0, 0.5]
@@ -31,10 +26,9 @@ class QuadraticEquation(TestCase):
 
     def test_random_parameters(self):
         for i in range(NUMBER_RANDOM_CASES):
-            # TODO: generate random a, b, c
             a, b, c = random.uniform(-10, 10), random.uniform(-10, 10), random.uniform(-10, 10)
             x1, x2 = equation_roots(a, b, c)
-            if x1 is not None:
+            if x1 is not None and x2 is not None:
                 equation_answer_1 = a * (x1 ** 2) + b * x1 + c
                 equation_answer_2 = a * (x2 ** 2) + b * x2 + c
-                self.assertAlmostEquals(equation_answer_1, equation_answer_2, 0)
+                self.assertAlmostEquals(equation_answer_1, equation_answer_2)
